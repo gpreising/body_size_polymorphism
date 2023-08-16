2b49c0d3783d971278cd87f97c9d57bdf52b84aa
Gabriel Preising <gabep@DNa1d04aa.SUNet>
2023-08-16 16:13:53 -0700

    initial commit for individual scripts, fixed bug in make_indiv_samtools_scripts.sh

--- scripts/make_indiv_samtools_scripts.sh
+++ scripts/make_indiv_samtools_scripts.sh
@@ -6,7 +6,7 @@ for f in data/split_reads/xmul_sample_*
 
 do
 
-        filename=${f}
-        echo -e "#!/bin/sh\n\n#SBATCH --ntasks=1\n#SBATCH --mem=64000\n#SBATCH --cpus-per-task=1\n#SBATCH -p normal,hns,schumer\n#SBATCH --time=48:00:00\n#SBATCH --job-name=${filename}\n\nml biology bwa samtools java bcftools\n\nperl /home/groups/schumer/shared_bin/Lab_shared_scripts/run_samtools_only_parental_v2.pl $filename /data/ref_genome_files/xmul-TABQ-V-22-M_v2023.1_forgabe_XY.fa PE" > scripts/samtools/${filename}.sh
+        filename=${f:17}
+        echo -e "#!/bin/sh\n\n#SBATCH --ntasks=1\n#SBATCH --mem=64000\n#SBATCH --cpus-per-task=1\n#SBATCH -p normal,hns,schumer\n#SBATCH --time=48:00:00\n#SBATCH --job-name=${filename}\n\nml biology bwa samtools java bcftools\n\nperl /home/groups/schumer/shared_bin/Lab_shared_scripts/run_samtools_only_parental_v2.pl data/split_reads/$filename data/ref_genome_files/xmul-TABQ-V-22-M_v2023.1_forgabe_XY.fa PE" > scripts/samtools/${filename}.sh
 
 done
---
+++ scripts/samtools/xmul_sample_cm.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+#SBATCH --ntasks=1
+#SBATCH --mem=64000
+#SBATCH --cpus-per-task=1
+#SBATCH -p normal,hns,schumer
+#SBATCH --time=48:00:00
+#SBATCH --job-name=xmul_sample_cm
+
+ml biology bwa samtools java bcftools
+
+perl /home/groups/schumer/shared_bin/Lab_shared_scripts/run_samtools_only_parental_v2.pl data/split_reads/xmul_sample_cm data/ref_genome_files/xmul-TABQ-V-22-M_v2023.1_forgabe_XY.fa PE
