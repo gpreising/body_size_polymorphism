8447dc355bc112ddaadb4e6a237ec19ef92ef71f
Gabriel Preising <gabep@DN0a25a918.SUNet>
2023-08-28 13:48:10 -0700

    swapped the argument order

--- scripts/submit_mpileup_scripts.sh
+++ scripts/submit_mpileup_scripts.sh
@@ -3,6 +3,6 @@ for f in scripts/bcftools/chr_*.sh
 do
 
         filename=${f:17}
-        sbatch scripts/bcftools/$filename data/readlist_PE data/all_case_control_20230823.txt 
+        sbatch scripts/bcftools/$filename data/all_case_control_20230823.txt data/readlist_PE
 
 done
