07a541b3717a1e49ee77a5d35a7e77657c3e10fe
Gabriel Preising <gabep@DN0a25a918.SUNet>
2023-08-28 13:45:55 -0700

    added arguments to the script

--- scripts/submit_mpileup_scripts.sh
+++ scripts/submit_mpileup_scripts.sh
@@ -3,6 +3,6 @@ for f in scripts/bcftools/chr_*.sh
 do
 
         filename=${f:17}
-        sbatch scripts/bcftools/$filename
+        sbatch scripts/bcftools/$filename data/readlist_PE data/all_case_control_20230823.txt 
 
 done
