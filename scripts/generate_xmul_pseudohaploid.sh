5ed6db120b08fe5e1838d354edd4e3770182a71f
Gabriel Preising <gabep@DN0a25a5b6.SUNet>
2023-09-01 12:13:30 -0700

    initial commit

---
+++ scripts/generate_xmul_pseudohaploid.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH --ntasks=1
+#SBATCH --mem=64000
+#SBATCH --cpus-per-task=1
+#SBATCH -p schumer
+#SBATCH --time=160:00:00
+#SBATCH --job-name=gen_pseudohaps
+
+ml biology samtools bcftools
+
+perl /home/groups/schumer/shared_bin/Lab_shared_scripts/generate_pseudohaploid_calls_from_bams.pl data/xmul_bamlist.txt data/ref_genome_files/xmul-TABQ-V-22-M_v2023.1_forgabe_XY.fasta
+data/readlist_PEindivs.allindiv.allchroms.vcf.summary.dedupheaders.filt.reformatted.gz
