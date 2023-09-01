3f4e84c92ae711efe22ccdae1859e30724230cd8
Gabriel Preising <gabep@DN0a25a5b6.SUNet>
2023-09-01 11:07:29 -0700

    fixed module load error

--- scripts/index_bgzip_focal_snps.sh
+++ scripts/index_bgzip_focal_snps.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-module load htslib
+ml biology htslib
 
 bgzip -c data/readlist_PEindivs.allindiv.allchroms.vcf.summary.dedupheaders.filt.reformatted > data/readlist_PEindivs.allindiv.allchroms.vcf.summary.dedupheaders.filt.reformatted.gz && tabix -s1 -b2 -e2 data/readlist_PEindivs.allindiv.allchroms.vcf.summary.dedupheaders.filt.reformatted.gz
