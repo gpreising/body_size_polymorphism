c84971a1b5b9a2a8077e4bd5c50cbcc37af6cbd1
Gabriel Preising <gabep@Gabriels-MacBook-Pro-2.local>
2023-09-02 13:17:47 -0700

    initial commit

---
+++ scripts/combine_pseudohaps.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#SBATCH --ntasks=1
+#SBATCH --mem=64000
+#SBATCH --cpus-per-task=1
+#SBATCH -p schumer
+#SBATCH --time=96:00:00
+#SBATCH --job-name=combine_pseudohaps
+
+perl /home/groups/schumer/shared_bin/Lab_shared_scripts/combine_pseudohaploid_files_pop_structure_analysis.pl data/xmul_pseudohap_list
