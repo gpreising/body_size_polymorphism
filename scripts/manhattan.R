88c0e1a25211f4ed7bda6a3ab2ca6b4e992b3cf9
Gabriel Preising <gabep@DN0a25a464.SUNet>
2023-09-12 10:34:14 -0700

    plotted mappability filtered GWAS

--- scripts/manhattan.R
+++ scripts/manhattan.R
@@ -30,3 +30,28 @@ dat_edited_Y <- dat_edited %>%
   filter(group == "21 Y")
 
 ggmanhattanPlot(dat_edited_Y, species="X. multilineatus", colors = c("#FCC000","grey"))
+
+# applied mappability filter
+dat_mapfilt <- read.csv("data/readlist_PEindivs.allindiv.allchroms.vcf.summary.dedupheaders.filt.mapfilt.bed",sep="\t", header=FALSE)
+
+dat_mapfilt <- dat_mapfilt %>%
+  select(-c(V3)) %>%
+  rename(group = V1,
+         pos = V2,
+         ref_allele = V4,
+         alt_allele = V5,
+         depth = V6,
+         AF_group1 = V7,
+         AF_group2 = V8,
+         LR1 = V9,
+         LR2 = V10,
+         pchi = V11)
+
+dat_mapfilt <- ggmanhattanFormat(dat_mapfilt)
+dat_mapfilt_edited <- dat_mapfilt %>%
+  mutate(group = str_extract(group, "(?<=_).*")) %>%
+  mutate(group = str_replace(group, "_", " ")) %>%
+  filter(depth >= 0.1 & (AF_group1 >= 0.05 | AF_group2 >= 0.05))
+
+
+
