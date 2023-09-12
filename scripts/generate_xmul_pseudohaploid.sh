3de0028852ea9ef4c7c404b5ecebbe351971e96f
Gabriel Preising <gabep@DN0a25a464.SUNet>
2023-09-12 11:33:23 -0700

    adjusted for mappability filtered data

--- scripts/generate_xmul_pseudohaploid.sh
+++ scripts/generate_xmul_pseudohaploid.sh
@@ -9,4 +9,4 @@
 
 ml biology samtools bcftools
 
-perl /home/groups/schumer/shared_bin/Lab_shared_scripts/generate_pseudohaploid_calls_from_bams.pl data/xmul_bamlist.txt data/ref_genome_files/xmul-TABQ-V-22-M_v2023.1_forgabe_XY.fasta data/readlist_PEindivs.allindiv.allchroms.vcf.summary.dedupheaders.filt.reformatted.gz
+perl /home/groups/schumer/shared_bin/Lab_shared_scripts/generate_pseudohaploid_calls_from_bams.pl data/xmul_bamlist.txt data/ref_genome_files/xmul-TABQ-V-22-M_v2023.1_forgabe_XY.fasta data/readlist_PEindivs.allindiv.allchroms.vcf.summary.dedupheaders.filt.mapfilt.reformatted.gz
