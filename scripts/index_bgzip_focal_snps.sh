eda7054330a5943879251a83d3d2f3afe8674878
Gabriel Preising <gabep@DN0a25a464.SUNet>
2023-09-12 11:24:23 -0700

    adjusted for mappability filtered data

--- scripts/index_bgzip_focal_snps.sh
+++ scripts/index_bgzip_focal_snps.sh
@@ -2,4 +2,4 @@
 
 ml biology htslib
 
-bgzip -c data/readlist_PEindivs.allindiv.allchroms.vcf.summary.dedupheaders.filt.reformatted > data/readlist_PEindivs.allindiv.allchroms.vcf.summary.dedupheaders.filt.reformatted.gz && tabix -s1 -b2 -e2 data/readlist_PEindivs.allindiv.allchroms.vcf.summary.dedupheaders.filt.reformatted.gz
+bgzip -c data/readlist_PEindivs.allindiv.allchroms.vcf.summary.dedupheaders.filt.mapfilt.reformatted > data/readlist_PEindivs.allindiv.allchroms.vcf.summary.dedupheaders.filt.mapfilt.reformatted.gz && tabix -s1 -b2 -e2 data/readlist_PEindivs.allindiv.allchroms.vcf.summary.dedupheaders.filt.mapfilt.reformatted.gz
