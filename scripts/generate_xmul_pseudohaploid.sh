368db1b82d8a89a333183e1ed142670b843dcc41
Gabriel Preising <gabep@DN0a23026e.SUNet>
2023-09-19 16:15:27 -0700

    edited to reflect filtering bug

--- scripts/generate_xmul_pseudohaploid.sh
+++ scripts/generate_xmul_pseudohaploid.sh
@@ -11,4 +11,4 @@ ml biology samtools bcftools perl
 
 cpanm Math::Random
 
-perl /home/groups/schumer/shared_bin/Lab_shared_scripts/generate_pseudohaploid_calls_from_bams.pl data/xmul_bamlist.txt data/ref_genome_files/xmul-TABQ-V-22-M_v2023.1_forgabe_XY.fasta data/readlist_PEindivs.allindiv.allchroms.vcf.summary.dedupheaders.filt.mapfilt.reformatted.gz
+perl /home/groups/schumer/shared_bin/Lab_shared_scripts/generate_pseudohaploid_calls_from_bams.pl data/xmul_bamlist.txt data/ref_genome_files/xmul-TABQ-V-22-M_v2023.1_forgabe_XY.fasta data/readlist_PEindivs.allindiv.allchroms.vcf.summary.dedupheaders.mapfilt.mafAFfilt.reformatted.gz
