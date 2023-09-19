ecc2f95bbe830eaf90b58a592fc326d096ea45e7
Gabriel Preising <gabep@DN0a23026e.SUNet>
2023-09-19 16:08:53 -0700

    edited for filtering bug

--- scripts/index_bgzip_focal_snps.sh
+++ scripts/index_bgzip_focal_snps.sh
@@ -2,4 +2,4 @@
 
 ml biology htslib
 
-bgzip -c data/readlist_PEindivs.allindiv.allchroms.vcf.summary.dedupheaders.filt.mapfilt.reformatted > data/readlist_PEindivs.allindiv.allchroms.vcf.summary.dedupheaders.filt.mapfilt.reformatted.gz && tabix -s1 -b2 -e2 data/readlist_PEindivs.allindiv.allchroms.vcf.summary.dedupheaders.filt.mapfilt.reformatted.gz
+bgzip -c data/readlist_PEindivs.allindiv.allchroms.vcf.summary.dedupheaders.mapfilt.mafAFfilt.reformatted > data/readlist_PEindivs.allindiv.allchroms.vcf.summary.dedupheaders.mapfilt.mafAFfilt.reformatted.gz && tabix -s1 -b2 -e2 data/readlist_PEindivs.allindiv.allchroms.vcf.summary.dedupheaders.mapfilt.mafAFfilt.reformatted.gz
