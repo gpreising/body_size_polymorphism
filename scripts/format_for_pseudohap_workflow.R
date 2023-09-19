7e51dc24f49e4f8875a06dca2fe982e5bf3a565a
Gabriel Preising <gabep@DN0a23026e.SUNet>
2023-09-19 16:04:28 -0700

    edited to reflect filtering bug

--- scripts/format_for_pseudohap_workflow.R
+++ scripts/format_for_pseudohap_workflow.R
@@ -14,11 +14,11 @@ focal_snps <- focal_snps %>%
 write.table(focal_snps,file="data/readlist_PEindivs.allindiv.allchroms.vcf.summary.dedupheaders.filt.reformatted", row.names=F, col.names=F, sep = "\t", quote = FALSE)
 
 # reformat the mappability-filtered data
-focal_mapfilt_snps <- read.csv("data/readlist_PEindivs.allindiv.allchroms.vcf.summary.dedupheaders.filt.mapfilt.bed",sep="\t", header=FALSE)
+focal_mapfilt_snps <- read.csv("data/readlist_PEindivs.allindiv.allchroms.vcf.summary.dedupheaders.mapfilt.bed",sep="\t", header=FALSE)
 
 focal_mapfilt_snps <- focal_mapfilt_snps %>%
   filter(V6 >= 0.1 & (V7 >= 0.05 | V8 >= 0.05)) %>%
   mutate(V12 = paste(V4,V5,sep=",")) %>%
   dplyr::select(V1,V2,V12)
 
-write.table(focal_mapfilt_snps,file="data/readlist_PEindivs.allindiv.allchroms.vcf.summary.dedupheaders.filt.mapfilt.reformatted", row.names=F, col.names=F, sep = "\t", quote = FALSE)
+write.table(focal_mapfilt_snps,file="data/readlist_PEindivs.allindiv.allchroms.vcf.summary.dedupheaders.mapfilt.mafAFfilt.reformatted", row.names=F, col.names=F, sep = "\t", quote = FALSE)
